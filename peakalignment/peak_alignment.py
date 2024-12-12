e90d67044cc0aa14e4ab7dd133b349b43292452b
WilliamDixon <52508794+WilliamDixon@users.noreply.github.com>
2024-12-12 16:33:56 -0500

    Fix align_peaks method

--- peakalignment/peak_alignment.py
+++ peakalignment/peak_alignment.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 from peakalignment.apply_alignment import apply_alignment
 from peakalignment.density_filter import get_density_filtered_matches
@@ -7,11 +8,12 @@ from peakalignment.signal_matching import get_matches
 
 
 def align_peaks(signal_a_peak_times: np.ndarray, signal_b_peak_times: np.ndarray, signal_a_times: np.ndarray,
-                max_offset=None):
+                max_offset=None, return_indices=False):
     match_indices_matrix, match_distance_matrix = get_matches(signal_a_peak_times, signal_b_peak_times, max_offset=max_offset)
 
-    offset_t, offset_v = get_density_filtered_matches(signal_a_peak_times, signal_b_peak_times, match_indices_matrix,
+    density_data = get_density_filtered_matches(signal_a_peak_times, signal_b_peak_times, match_indices_matrix,
                                                       max_offset=max_offset)
+    offset_t, offset_v = density_data['fine_offset_t'], density_data['fine_offset_v']
 
     line_data = get_regression_lines(offset_t, offset_v)
 
@@ -19,5 +21,7 @@ def align_peaks(signal_a_peak_times: np.ndarray, signal_b_peak_times: np.ndarray
         apply_alignment(signal_a_times, line_data)
 
     sorted_indices = alignment_corrected_times.argsort()
+    if return_indices:
+        return sorted_indices, aligned_original_times[sorted_indices], aligned_indices[sorted_indices]
 
     return alignment_corrected_times[sorted_indices]
